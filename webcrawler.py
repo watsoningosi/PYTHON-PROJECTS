3b27246c6c942db20f56cb88bd739234b1fd9ed4
watsoningosi <watstarsparta@gmail.com>
2022-03-31 16:28:28 +0300

    pilot commit

--- webcrawler.py
+++
@@ -1,5 +0,0 @@
-import scrapy
-class spider1(scrapy.Spider):
-        start_urls = [‘https://en.wikipedia.org/wiki/Battery_(electricity)’]       
-        def parse(self, response):
-           pass
